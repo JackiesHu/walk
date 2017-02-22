36e678a10d19ea2fab7428df696515638a571e0e
zhaokun <zhaokun@quanjing>
2017-02-22 21:44:24 +0800

    地图定位路况

--- app/src/main/java/com/buxingzhe/pedestrian/activity/MainActivity.java
+++ app/src/main/java/com/buxingzhe/pedestrian/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.buxingzhe.pedestrian.R;
 import com.buxingzhe.pedestrian.found.FoundFragment;
@@ -23,7 +24,7 @@ public class MainActivity extends BaseActivity implements  View.OnClickListener 
 
     private List<Fragment> fragments = new ArrayList<Fragment>();
     private MQJMainPagerAdapter pagerAdapter;
-
+    private ImageView vRun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +36,7 @@ public class MainActivity extends BaseActivity implements  View.OnClickListener 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         vTabbar = (MWTTabBar) findViewById(R.id.TabBar);
+        vRun = (ImageView) findViewById(R.id.main_run);
         initTabbar();
         construct();
         pagerAdapter = new MQJMainPagerAdapter(mContext,getSupportFragmentManager(), fragments);
@@ -74,18 +76,24 @@ public class MainActivity extends BaseActivity implements  View.OnClickListener 
         FoundFragment mFoundFragment2 = new FoundFragment();
         FoundFragment mFoundFragment3 = new FoundFragment();
         FoundFragment mFoundFragment4 = new FoundFragment();
+        FoundFragment mFoundFragment5 = new FoundFragment();
 
         fragments.add(mFoundFragment1);
         fragments.add(mFoundFragment2);
         fragments.add(mFoundFragment3);
         fragments.add(mFoundFragment4);
-
+        fragments.add(mFoundFragment5);
 
     }
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        adapter.switchView(id);
+        if (id == 2){
+            vRun.setImageResource(R.mipmap.ic_run_press);
+        }else {
+            vRun.setImageResource(R.mipmap.ic_run_nor);
+            adapter.switchView(id);
+        }
         mViewPager.setCurrentItem(id);
     }
 }
