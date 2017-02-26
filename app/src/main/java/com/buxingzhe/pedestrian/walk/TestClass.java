898d724a2d3d71b04892c01aa0f4c4f29096768f
zhaokun <zhaokun@quanjing>
2017-02-26 14:20:19 +0800

    新增WalkedFragment

--- app/src/main/java/com/buxingzhe/pedestrian/walk/TestClass.java
+++
@@ -1,7 +0,0 @@
-package com.buxingzhe.pedestrian.walk;
-
-/**
- * Created by quanjing on 2017/2/26.
- */
-public class TestClass {
-}
---
+++ app/src/main/java/com/buxingzhe/pedestrian/walk/WalkedFragment.java
@@ -0,0 +1,40 @@
+package com.buxingzhe.pedestrian.walk;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.buxingzhe.pedestrian.R;
+import com.buxingzhe.pedestrian.activity.BaseFragment;
+import com.buxingzhe.pedestrian.widget.TitleBarView;
+
+/**
+ * Created by quanjing on 2017/2/23.
+ */
+public class WalkedFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout_walk,container,false);
+        mContext = getContext();
+        findId(view);
+        onClick();
+        return view;
+    }
+    private void findId(View view){
+        vTitleBar = (TitleBarView) view.findViewById(R.id.walk_title_bar);
+        setTitle("步行者联盟");
+        hideLeftIco();
+    };
+    private void onClick() {
+
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+    }
+}
